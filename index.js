c0581b81ad2a46a1bda8eccd123353f7f6cdc980
Jeanpierre Montalvo Velasquez <jeanpierre_montalvo@usmp.pe>
2022-08-13 20:02:06 -0500

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
+//routes
+//app.get('/api/users',(req,res) => res.json([{name:'ryan'},{name:"joe"}]))
+//app.use(express.)get('/',(req, res) => res.send("hello world"))
 
+
+//static files  
+app.use(express.static('public'))
+
+//starting server
 app.listen(process.env.PORT || 3000)
 console.log('Server on port', process.env.PORT ||3000)
